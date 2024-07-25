332521340469e9a7a4ba09a7cafcc48a03ccbca6
Anurag Chauhan <anuragchauhan4455@gmail.com>
2024-07-25 15:15:05 +0530

    comment edit

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function beforeSubmit(event){
         let outputDate = document.querySelector(".outputDate");
         let inputDate = document.querySelector(".inputDate");
         console.log("inputDate.value", inputDate.value);
-        // string ---> date  (en_IN) you can get your local from devloper console anonymus window system.debug(UserInfo.getLocale());
+        // string ---> date  (en_IN) you can get your local from devloper console anonymus window 
+        //system.debug(UserInfo.getLocale());
 
         let formatedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
         outputDate.value = formatedDate;
